5301a769481dc238c236cf5d7ad2db4019733114
mkleemoff <mkleemjr@gmail.com>
2020-03-10 20:26:49 -0400

    Added componentDidMount and shouldComponentUpdate. Tested and working.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class Ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  };
+
+  // Lifecycle methods
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+      })
+    }, 1000);
+  }
+
+  // Reset method
+  reset = () => {
+    this.setState({
+      count: 0,
+    })
+  };
+
+  shouldComponentUpdate (nextProps, nextState) {
+    // this is where we check a condition if it's true or false
+    // and only allow a rerender if the condition is true
+    return (nextState.count % 3 === 0) 
+  }
+  
+  render() {
+    return  (
+      <div>
+        <div>The ticker is: {this.state.count}</div>
+        <div>
+          <button type="button" onClick={this.reset}>Reset counter</button>
+        </div>
+      </div>
+    )
+  };
+};
+
+export default Ticker;
